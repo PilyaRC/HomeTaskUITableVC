2782984e5f696124a285fd01ded33c557845aa74
Roman Piliavskyi <chernosliv.billy@gmail.com>
2021-04-28 21:48:26 +0300

    Initial Commit

---
+++ UITableVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableVC
+//
+//  Created by PiliavskyiMBP on 28.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
