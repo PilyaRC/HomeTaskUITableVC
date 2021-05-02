bffcc51e5097845d82d87c3f2a7bcdee2cd2b1e5
Roman Piliavskyi <chernosliv.billy@gmail.com>
2021-05-02 04:05:57 +0300

    Create custom cell

--- UITableVC/ViewController.swift
+++ UITableVC/ViewController.swift
@@ -7,13 +7,147 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Section {
+    let title: String
+    let options: [SettingsOptionType]
+}
+
+enum SettingsOptionType {
+    case staticCell(model: SettingsOption)
+    case switchCell(model: SettingsSwitchOption)
+}
+
+struct SettingsSwitchOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+    var isOn: Bool
+}
+
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var models = [Section]()
+
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifier)
+        table.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifier)
+
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configure()
+        title = "Настройки"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
 
+    func configure() {
+        models.append(Section(title: "General", options: [
+            .switchCell(model: SettingsSwitchOption(title: "Авиарежим", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemOrange, handler:  {
 
+            }, isOn: false)),
+
+            .staticCell(model: SettingsOption(title: "Bluetooth", icon: UIImage(systemName: "command"), iconBackgroundColor: .link) {
+
+            }),
+            .staticCell(model: SettingsOption(title: "Wi-Fi", icon: UIImage(systemName: "wifi"), iconBackgroundColor: .link) {
+            }),
+
+            .staticCell(model: SettingsOption(title: "Сотовая связь", icon: UIImage(systemName: "antenna.radiowaves.left.and.right"), iconBackgroundColor: .systemGreen) {
+            }),
+
+            .staticCell(model: SettingsOption(title: "Режим модема", icon: UIImage(systemName: "personalhotspot"), iconBackgroundColor: .systemGreen) {
+            }),
+
+            .switchCell(model: SettingsSwitchOption(title: "VPN", icon: UIImage(systemName: "vpn"), iconBackgroundColor: .link, handler:  {
+
+            }, isOn: false))
+        ]))
+
+        models.append(Section(title: "Information", options: [
+            .staticCell(model: SettingsOption(title: "Уведомления", icon: UIImage(systemName: "arrow.rectanglepath"), iconBackgroundColor: .systemRed) {
+
+            }),
+            .staticCell(model: SettingsOption(title: "Звуки, тактильные сигналы", icon: UIImage(systemName: "speaker.wave.3.fill"), iconBackgroundColor: .systemPink) {
+
+            }),
+            .staticCell(model: SettingsOption(title: "Не беспокоить", icon: UIImage(systemName: "powersleep"), iconBackgroundColor: .systemPurple) {
+            }),
+            .staticCell(model: SettingsOption(title: "Экранное время", icon: UIImage(systemName: "hourglass"), iconBackgroundColor: .systemPurple) {
+            }),
+
+
+        ]))
+
+        models.append(Section(title: "Apps", options: [
+            .staticCell(model: SettingsOption(title: "Основные", icon: UIImage(systemName: "gear"), iconBackgroundColor: .systemGray) {
+
+            }),
+            .staticCell(model: SettingsOption(title: "Пункт управления", icon: UIImage(systemName: "switch.2"), iconBackgroundColor: .systemGray) {
+
+            }),
+            .staticCell(model: SettingsOption(title: "Экран и яркость", icon: UIImage(systemName: "textformat.size"), iconBackgroundColor: .link) {
+            }),
+            .staticCell(model: SettingsOption(title: "Экран «Домой»", icon: UIImage(systemName: "calendar.badge.plus"), iconBackgroundColor: .link) {
+            }),
+
+            .staticCell(model: SettingsOption(title: "Универсальный доступ", icon: UIImage(systemName: "figure.wave.circle"), iconBackgroundColor: .link) {
+            }),
+        ]))
+    }
+
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        let section = models[section]
+//        return section.title
+//    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return models[section].options.count
+}
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.section].options[indexPath.row]
+
+        switch model.self {
+        case .staticCell(let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier, for: indexPath) as? SettingsTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
+        case .switchCell(let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SwitchTableViewCell.identifier, for: indexPath) as? SwitchTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
+        }
+}
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let type = models[indexPath.section].options[indexPath.row]
+        switch type.self {
+        case .staticCell(let model):
+            model.handler()
+        case .switchCell(let model):
+            model.handler()
+        }
+    }
 }
 
